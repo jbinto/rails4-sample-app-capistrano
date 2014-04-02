a909894948419e8f0db2595e1b076e3a45fdc730
Jesse Buchanan <jbinto@jbinto.ca>
2014-04-02 17:02:52 -0400

    Add many tasks from Capistrano 3 Tutorial
    http://www.talkingquickly.co.uk/2014/01/deploying-rails-apps-to-a-vps-with-capistrano-v3/

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,58 +1,65 @@
-# config valid only for Capistrano 3.1
+# Adapted from:
+#
+# http://www.talkingquickly.co.uk/2014/01/deploying-rails-apps-to-a-vps-with-capistrano-v3/
+# https://github.com/TalkingQuickly/capistrano-3-rails-template
+
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'sampleapp'
+set :deploy_user, 'deploy'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :scm, :git
+set :repo_url, 'git@github.com:jbinto/sample_app_4_0_upgrade.git'
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
 
-# Default value for :scm is :git
-# set :scm, :git
+# See https://github.com/capistrano/rbenv#usage for details
+# on rbenv boilerplate.
 
-# Default value for :format is :pretty
-# set :format, :pretty
+# I believe this should be 'user', since it's installed as ~deploy
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+set :keep_releases, 5
 
-# Default value for :pty is false
-# set :pty, true
+# These are symlinks, so redeploys don't "overwrite" important files.
+# Not sure why app/database not updated. I get logs, sockets, pids, etc.
 
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# Run Rspec tests before deploying. Nifty.
+# Depends on lib/capistrano/tasks/run_tests.cap
+set :tests, ["spec"]
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+## Config files to be copied by deploy:setup_config
+## Depends on lib/capistrano/tasks/setup_config.cap
+set(:config_files, %w(
+  nginx.conf
+  database.example.yml
+))
+
+set(:executable_config_files, [])
+
+# files which need to be symlinked to other parts of the
+# filesystem. For example nginx virtualhosts, log rotation
+# init scripts etc.
+set(:symlinks, [
+  {
+    source: "nginx.conf",
+    link: "/etc/nginx/sites-enabled/#{fetch(:full_app_name)}"
+  }
+])
+
+
+set :keep_releases, 5
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
+  before :deploy, "deploy:check_revision"
+  before :deploy, "deploy:run_tests"
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
 end
